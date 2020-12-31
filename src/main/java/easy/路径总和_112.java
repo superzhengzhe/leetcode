67bd0d4a13662a6c58e18dae1e6dcc46fcbdcc55
superzhengzhe <792022511@qq.com>
2020-12-31 16:37:51 +0800

    路径总和
    路径总和

---
+++ src/main/java/easy/路径总和_112.java
@@ -0,0 +1,42 @@
+package easy;
+
+import base.TreeNode;
+
+/**
+ * 路径总和
+ * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 示例: 
+ * 给定如下二叉树，以及目标和 sum = 22，
+ *
+ *               5
+ *              / \
+ *             4   8
+ *            /   / \
+ *           11  13  4
+ *          /  \      \
+ *         7    2      1
+ * 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+ * @author zz
+ * @date 2020/12/31 14:30
+ */
+public class 路径总和_112 {
+
+    /**
+     * 做法：dfs
+     * 深度优先搜索，每次访问一条路径（从根节点到叶子节点），如果不满足，则继续寻找下一条
+     * @param root
+     * @param sum
+     * @return
+     */
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == sum;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+
+}

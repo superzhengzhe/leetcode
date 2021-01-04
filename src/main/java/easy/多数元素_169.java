7767bb972359e4ceafba5ec34a357444e8593d3a
superzhengzhe <792022511@qq.com>
2021-01-04 11:50:58 +0800

    多数元素
    多数元素

---
+++ src/main/java/easy/多数元素_169.java
@@ -0,0 +1,67 @@
+package easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 多数元素
+ * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数 大于 n/2 的元素。
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ * 示例 1：
+ * 输入：[3,2,3]
+ * 输出：3
+ * 示例 2：
+ * 输入：[2,2,1,1,1,2,2]
+ * 输出：2
+ * @author zz
+ * @date 2021/1/4 10:00
+ */
+public class 多数元素_169 {
+
+    /**
+     * 做法1：哈希表统计次数
+     * @param nums
+     * @return
+     */
+    public int majorityElement0(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0;i < nums.length;i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], 1);
+            } else {
+                int count = map.get(nums[i]);
+                if (++count > nums.length / 2) {
+                    return nums[i];
+                }
+                map.put(nums[i], count);
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 做法2：排序取众数
+     * @param nums
+     * @return
+     */
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1};
+        System.out.println(new 多数元素_169().majorityElement(nums));
+
+        nums = new int[]{3,2,3};
+        System.out.println(new 多数元素_169().majorityElement(nums));
+
+        nums = new int[]{2,2,1,1,1,2,2};
+        System.out.println(new 多数元素_169().majorityElement(nums));
+    }
+
+}

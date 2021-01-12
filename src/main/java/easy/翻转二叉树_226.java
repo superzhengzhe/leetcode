85ca6048896d573ef50d4fd7777e0c694a6a5918
superzhengzhe <792022511@qq.com>
2021-01-12 14:53:20 +0800

    翻转二叉树
    翻转二叉树

---
+++ src/main/java/easy/翻转二叉树_226.java
@@ -0,0 +1,46 @@
+package easy;
+
+import base.TreeNode;
+
+/**
+ * 翻转二叉树
+ * 翻转一棵二叉树。
+ * 示例：
+ * 输入：
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 输出：
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ * 备注:
+ * 这个问题是受到 Max Howell 的 原问题 启发的 ：
+ * 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+ * @author zz
+ * @date 2021/1/12 14:43
+ */
+public class 翻转二叉树_226 {
+
+    /**
+     * 做法：递归
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+}
